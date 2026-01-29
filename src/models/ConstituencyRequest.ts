334aca62cd369ee0d4f007e8e0dcc277979d956b
SIRIWAT DAMRONGTAMWATTANA <99265@icc.co.th>
2026-01-29 16:16:29 +0700

    fix fils ConstituencyRequest

---
+++ src/models/ConstituencyRequest.ts
@@ -0,0 +1,9 @@
+export interface ConstituencyRequest {
+          
+          consituencynumber: number;
+          address: string; 
+          district: string;
+          subdistrict: string;
+          province: string;
+          zipcode: string;
+}
