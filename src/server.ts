789e990786079756f38327d71b0492148ac97aca
SIRIWAT DAMRONGTAMWATTANA <99265@icc.co.th>
2026-02-07 19:17:03 +0700

    add port 5000 for deploy

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import voterouter from './routes/VoteRoutes'
 import partyRoutes from './routes/PartyRoutes';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors())
 
