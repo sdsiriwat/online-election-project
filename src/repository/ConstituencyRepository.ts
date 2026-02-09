245a27769751bc7cfbd64f0d17355daf2b74e4fe
SIRIWAT DAMRONGTAMWATTANA <99265@icc.co.th>
2026-02-09 15:40:50 +0700

    feat(Constituency):  Create AIP for Manage system Constituency(create,get delete and open or close Constituency)

--- src/repository/ConstituencyRepository.ts
+++ src/repository/ConstituencyRepository.ts
@@ -19,3 +19,83 @@ export async function createConstituency(
         }
     });
 }
+
+export async function getAllConstituencies() {
+    return prisma.consituency.findMany();
+}
+
+export async function findConstituencyById(id: number) {
+    return prisma.consituency.findUnique({
+        where: {
+            id: id,
+        },
+    });
+}
+
+export async function updateConstituency(id: number, 
+    province: string,
+    consituencynumber: number,
+    subdistrict: string,
+    district: string,
+    zipcode: string,
+) {
+    return prisma.consituency.update({
+        where: {
+            id: id,
+        },
+        data: {
+            province,
+            consituencynumber,
+            subdistrict,
+            district,
+            zipcode,
+        },
+    });
+}
+
+export async function deleteConstituency(id: number) {
+    return prisma.consituency.delete({
+        where: {
+            id: id,
+        },
+    });
+}
+
+export async function openConstituencyElection(id: number) {
+    return prisma.consituency.update({
+        where: {
+            id: id,
+        },
+        data: {
+            isclosed: false,
+        },
+    });
+}
+
+export async function closeConstituencyElection(id: number) {
+    return prisma.consituency.update({
+        where: {
+            id: id,
+        },
+        data: {
+            isclosed: true,
+        },
+    });
+}
+
+
+export async function openConstituencyElectionAll() {
+    return prisma.consituency.updateMany({
+        data: {
+            isclosed: false,
+        },
+    });
+}
+
+export async function closeConstituencyElectionAll() {
+    return prisma.consituency.updateMany({
+        data: {
+            isclosed: true,
+        },
+    });
+}
